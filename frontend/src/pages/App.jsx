1128dce0e9f8af0cf7894e2740830e2bdbe80ded
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-10-07 20:26:54 -0300

    Nueva estructura de app con React & Tailwind CSS

---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,19 @@
+import '../../src/styles/App.css';
+import React from 'react';
+
+// Componentes a importar
+// import {} from "../components"
+
+
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='caret-amber-600'>TaskFlow</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
