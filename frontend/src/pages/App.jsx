366305872ad4f0d8adb86b95361866631309a333
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-10-16 10:08:22 -0300

    Armando estructura nueva de TaskFlow

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,19 +1,16 @@
 import '../../src/styles/App.css';
 import React from 'react';
 
-// Componentes a importar
-// import {} from "../components"
-
-
-
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='caret-amber-600'>TaskFlow</h1>
+        <h1 className='title'>TaskFlow</h1>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
