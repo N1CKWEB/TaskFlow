adc504b25546879a25be1993e9235891d9a60776
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-10-20 12:42:36 -0300

    Nuevos cambios de modificaciones

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,47 +1,140 @@
 import '../../src/styles/App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineTeam } from "react-icons/ai";
+import { GoProjectSymlink } from "react-icons/go";
+import { IoSettings } from "react-icons/io5";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import userImg from '../assets/img/img-logo-perfil-user-new.png';
+import { RiUserStarFill } from "react-icons/ri";
+import { GrUserManager } from "react-icons/gr";
+import { LiaUserPlusSolid } from "react-icons/lia";
+import { TbUserCode } from "react-icons/tb";
 
 export function Home() {
-  return (
-    <div className="App">
 
-      <header className="App-header">
-        <h1 className='title'>TaskFlow</h1>
-        <div>
-        <img src="" alt=""  className='img-logo-principal'/>
+// const [card,setCard]=useState(0)
+
+// const text=setCard ? '+ Nuevo Proyecto' : 'Importar Proyecto'
+
+async function crearProyecto() {
+  
+}
+
+ return (
+<div className="layout">
+
+  <aside className="sidebar">
+    {/* Logo */}
+    <h2 className="sidebar-title">TaskFlow</h2>
+    <hr className='sidebar-line' />
+    {/* Menú */}
+    <nav className="menu">
+      
+      <a href=''>Proyecto
+      <GoProjectSymlink className='icons'/>
+      </a>
+      
+      <a href=''>Equipo
+      <AiOutlineTeam className='icons'/>
+      </a>
+
+      <a href=''>Ajustes
+      <IoSettings className='icons' />
+      </a>
+
+    </nav>
+
+    {/* Usuario y logout */}
+    <div className="user-box">
+      <button className="logout-button">
+        {/* Podés agregar un ícono si querés */}
+        <RiLogoutBoxRLine className='icon-logout'/>
+        Cerrar Sesión
+      </button>
+      <div className="user-info">
+        <img
+          src={userImg} // reemplazá por la foto real
+          className="user-avatar"
+        />
+
+        <div className="user-texts">
+          <p className="title-user-box">Nombre Usuario</p>
+          <p className="subtitle-user-box">Líder de Proyecto</p>
         </div>
-        {/* Logo de Perfil */}
-        <div className='container-logo-perfil'>
-          <a href="" className='link-perfil'>
-        <img src="" alt="" className='img-logo-perfil' />
-        </a>
-        </div>
-      </header>
-
-      <section className='App-section'>
-
-        <div className='container-home'>
-          <h2 className='name-home'>Hola Nicolás</h2>
-          <h4 className='subtitle-home'>Bienvenido de vuelta a tu espacio de tareas para administrar</h4>
-
-         {/* Buscador de proyecto */}
-          <input placeholder='Introduce el nombre del proyecto' className='container-search' type="text" />
-
-         <div className='container-principal-project'>
-          <h3 className='name-project'>Proyectos</h3>
-          
-          {/* Boton crear un nuevo proyecto */}
-          <button className='button-project'>+ Nuevo Proyecto</button>
-
-          {/* Este sera el div de los proyectos*/}
-          <div className='container-projects-1'>
-          </div>
-
-
-         </div>
-        </div>
-      </section>
+      </div>
     </div>
+
+  </aside>
+
+  <main className="content">
+    {/* Saludo */}
+    <h1 className="title">Hola Nicolás</h1>
+    <p className="subtitle">¡Bienvenido de nuevo al espacio de trabajo, te extrañamos!</p>
+
+    {/* Buscador */}
+    <input
+      type="text"
+      className="search-input"
+      placeholder="Buscar proyecto...."
+    />
+
+    {/* Lista de proyectos */}
+    {
+      <div className="projects-container">
+      {/* cada tarjeta de proyecto */}
+    </div>
+    }
+
+    {/* Botones */}
+      <h3 className='title-actions' >Proyectos</h3>
+    <div className="actions">
+      {
+        <button onClick={()=> console.log("Creando proyecto")} className="new-btn">+ Nuevo Proyecto</button>
+      }
+      <button className="import-btn">Importar Proyecto</button>
+    
+      <div className='card-new-project'>
+      <h2>Titulo del Proyecto</h2>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Introduce el nombre del proyecto"
+        />
+        <h2>Miembros del equipo</h2>
+      <RiUserStarFill />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Introduce el nombre del dueño del proyecto"
+        />
+
+      <GrUserManager />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Introduce el nombre del lider técnico"
+        />
+      <TbUserCode />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Introduce el nombre del desarrollador"
+        />
+      
+      <h3>Agregar otro desarrollador</h3>
+      <LiaUserPlusSolid />
+
+
+
+
+
+
+      </div>
+    </div>
+  </main>
+
+</div>
+
   );
 }
 
